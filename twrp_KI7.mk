d711a69db0b7363b8e0bf7fdf99e02c8240e50df
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-03-08 02:37:09 -0800

    naming

--- twrp_KI7.mk
+++ twrp_KI7.mk
@@ -10,20 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-KI7 device
-$(call inherit-product, device/tecno/TECNO-KI7/device.mk)
+$(call inherit-product, device/tecno/KI7/device.mk)
 
-PRODUCT_DEVICE := TECNO-KI7
-PRODUCT_NAME := omni_TECNO-KI7
+PRODUCT_DEVICE := KI7
+PRODUCT_NAME := twrp_TECNO-KI7
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KI7
 PRODUCT_MANUFACTURER := tecno
 
 PRODUCT_GMS_CLIENTID_BASE := android-tecno
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_ki7_v7510-user 12 SP1A.210812.016 680176 release-keys"
-
-BUILD_FINGERPRINT := TECNO/KI7-GL/TECNO-KI7:12/SP1A.210812.016/240814V3288:user/release-keys
